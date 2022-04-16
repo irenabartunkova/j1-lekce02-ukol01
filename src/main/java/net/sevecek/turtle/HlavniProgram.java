8df4eb0915f3cebf0b79592a97735ce800e1b801
irenabartunkova <irena.bartunkova@gmail.com>
2022-04-16 18:52:51 +0200

    Želva nakreslí obrázek s podpisem.

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -61,6 +61,54 @@ public class HlavniProgram {
         zofka.move(130.0);
         zofka.turnRight(90.0);
         zofka.move(200.0);
+        zofka.turnRight(90.0);
+        zofka.penDown();
+
+        nakresliPismenoI(zofka);
+
+        zofka.penUp();
+        zofka.turnRight(90.0);
+        zofka.move(50.0);
+        zofka.turnRight(90.0);
+        zofka.move(60.0);
+        zofka.turnLeft(180.0);
+        zofka.penDown();
+
+        nakresliPismenoR(zofka);
+
+        zofka.penUp();
+        zofka.turnLeft(60.0);
+        zofka.move(50.0);
+        zofka.turnLeft(90.0);
+        zofka.penDown();
+
+        nakresliPismenoE(zofka);
+
+        zofka.penUp();
+        zofka.move(50.0);
+        zofka.turnLeft(90.0);
+        zofka.penDown();
+
+        nakresliPismenoN(zofka);
+
+        zofka.penUp();
+        zofka.turnRight(90.0);
+        zofka.move(50.0);
+        zofka.turnRight(90.0);
+        zofka.move(60.0);
+        zofka.turnLeft(180.0);
+        zofka.penDown();
+
+        nakresliPismenoA(zofka);
+
+        zofka.penUp();
+        zofka.turnRight(180.0);
+        zofka.move(200.0);
+
+
+
+
+
 
 
 
@@ -204,7 +252,61 @@ public class HlavniProgram {
         zofka.penDown();
 
     }
+    private void nakresliPismenoI(Turtle zofka) {
+        zofka.move(60.0);
 
+}
+    private void nakresliPismenoR(Turtle zofka) {
+        zofka.move(60.0);
+        zofka.turnRight(90.0);
+        for (int i = 0; i < 9; i++) {
+            zofka.move(5.0);
+            zofka.turnRight(20.0);
+                    }
+        zofka.turnLeft(120.0);
+        zofka.move(35.0);
+
+    }
+    private void nakresliPismenoE(Turtle zofka) {
+        zofka.move(60.0);
+        zofka.turnRight(90.0);
+        zofka.move(25.0);
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(25);
+        zofka.turnLeft(90.0);
+        zofka.move(30.0);
+        zofka.turnLeft(90.0);
+        zofka.penDown();
+        zofka.move(25.0);
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(25);
+        zofka.turnLeft(90.0);
+        zofka.move(30.0);
+        zofka.turnLeft(90.0);
+        zofka.penDown();
+        zofka.move(25.0);
+}
+    private void nakresliPismenoN(Turtle zofka) {
+        zofka.move(60.0);
+        zofka.turnRight(150.0);
+        zofka.move(70.0);
+        zofka.turnLeft(150.0);
+        zofka.move(60.0);
+            }
+    private void nakresliPismenoA(Turtle zofka) {
+        zofka.turnRight(25.0);
+        zofka.move(65.0);
+        zofka.turnRight(135.0);
+        zofka.move(65.0);
+        zofka.turnRight(180.0);
+        zofka.move(30.0);
+        zofka.turnLeft(70);
+        zofka.move(25.0);
+
+
+    }
 }
 
 
