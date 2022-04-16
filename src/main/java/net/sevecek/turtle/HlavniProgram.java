fd719a3d5175316c20412d80df50213fcd82b526
irenabartunkova <irena.bartunkova@gmail.com>
2022-04-16 14:47:42 +0200

    Želva nakreslí osmiúhelník, kolečko a sluníčko.

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,25 +8,48 @@ public class HlavniProgram {
         Turtle zofka;
         zofka = new Turtle();
         nakresliPrasatko(zofka);
+         zofka.penUp();
+         zofka.turnRight(135.0);
+         zofka.move(200.0);
+         zofka.turnRight(90.0);
+         zofka.move(400.0);
+
+         zofka.penDown();
+
+        nakresliOsmiuhelnik(zofka);
+
+        zofka.penUp();
+        zofka.turnRight(180.0);
+        zofka.move(130.0);
+
+        zofka.penDown();
+        nakresliKolecko(zofka);
+
+        zofka.penUp();
+        zofka.move(180.0);
+        zofka.penDown();
+
+        nakresliSlunicko(zofka);
+
    }
 
    private void nakresliPrasatko(Turtle zofka) {
         for (int i = 0; i < 4; i++) {
-            zofka.move(150.0);
+            zofka.move(120.0);
             zofka.turnRight(90.0);
         }
 
         zofka.turnLeft(60.0);
         zofka.penDown();
-        zofka.move(150.0);
+        zofka.move(120.0);
         zofka.turnRight(120.0);
-        zofka.move(150.0);
+        zofka.move(120.0);
 
         zofka.turnRight(30.0);
         zofka.penUp();
-        zofka.move(150.0);
+        zofka.move(120.0);
         zofka.turnRight(90.0);
-        zofka.move(150.0);
+        zofka.move(120.0);
         zofka.turnRight(90.0);
         zofka.penDown();
 
@@ -44,7 +67,7 @@ public class HlavniProgram {
 
         zofka.penUp();
         zofka.turnLeft(135.0);
-        zofka.move(150.0);
+        zofka.move(120.0);
         zofka.penDown();
         zofka.turnLeft(135.0);
         zofka.move(40.0);
@@ -59,7 +82,37 @@ public class HlavniProgram {
         zofka.move(40.0);
 
         }
+     private void nakresliOsmiuhelnik(Turtle zofka) {
+         for (int i = 0; i < 8; i++) {
+             zofka.move(50.0);
+             zofka.turnRight(45.0);
+         }
 
-  }
+     }
+    private void nakresliKolecko(Turtle zofka) {
+
+        for (int i = 0; i < 24; i++) {
+            zofka.move(15.0);
+            zofka.turnLeft(15.0);
+        }
+     }
+
+    private void nakresliSlunicko(Turtle zofka) {
+
+        for (int i = 0; i < 12; i++) {
+            zofka.move(30.0);
+            zofka.turnRight(80.0);
+            zofka.move(25.0);
+            zofka.penUp();
+            zofka.turnLeft(180.0);
+            zofka.move(25.0);
+            zofka.turnRight(100.0);
+            zofka.penDown();
+                      zofka.turnLeft(30.0);
+        }
+    }
+}
+
+
 
 
